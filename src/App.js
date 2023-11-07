28b452dbbd3924ec413b10bca55c541808464f7f
Wajola55 <koczorwioletta@gmail.com>
2023-11-07 14:15:04 +0000

    Update footer layout

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
-// src/App.js
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from './components/Navbar';
 import SearchBar from './components/SearchBar';
 import ProductList from './components/ProductList';
 import Intro from './components/Intro';
 import Categories from './components/Categories';
-// import ParentComponent from './components/ParentComponent';
-import './App.css'
-
-
+import Footer from './components/Footer';
+import './App.css';
 
 function App() {
   const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetchInitialProducts();
+  }, []);
+
+  const fetchInitialProducts = () => {
+    const requestBody = {
+      page: 1,
+      page_size: 30,
+    };
+
+    fetch('/publisher/products/', {
+      method: 'POST',
+      headers: {
+        'Authorization': `Token ${process.env.REACT_APP_API_TOKEN}`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(requestBody)
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log("Fetched initial products:", data.data);
+      setProducts(data.data);
+    })
+    .catch(error => {
+      console.error('Error fetching initial products:', error);
+    });
+  };
 
   const fetchProductsByCategory = (category) => {
     console.log("Fetch products for category:", category);
@@ -54,6 +79,8 @@ function App() {
       <Categories onSelectCategory={fetchProductsByCategory} />
       <SearchBar onSearch={handleSearch} />
       <ProductList products={products} />
+      <Footer />
+
     </div>
   );
 }
--- src/components/ParentComponent.js
+++ src/components/ParentComponent.js
@@ -5,28 +5,21 @@ import Categories from './Categories';
 
 const ParentComponent = () => {
   const [products, setProducts] = useState([]);
-  // const [selectedCategory, setSelectedCategory] = useState(''); // Unused variable
-  // const [searchQuery, setSearchQuery] = useState(''); // Unused variable
+
 
   const fetchProductsBySearch = (searchData) => {
-    // Implementation
     setProducts(searchData);
   };
 
   const fetchProductsByCategory = (category) => {
     console.log("Fetch products for category:", category);
-    // You should implement the logic to fetch products based on the category
-    // For example, you might set the products state with the fetched data
   };
 
   const handleSearch = (searchData) => {
-    // setSelectedCategory(''); // Comment out if you're not using it
     fetchProductsBySearch(searchData);
   };
 
   const handleSelectCategory = (category) => {
-    // setSearchQuery(''); // Comment out if you're not using it
-    // setSelectedCategory(category); // Comment out if you're not using it
     fetchProductsByCategory(category);
   };
 
