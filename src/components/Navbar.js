4ab93a908d740a6b9e65770132eb31ef652bed45
Wajola55 <koczorwioletta@gmail.com>
2023-12-01 18:22:26 +0100

    Implement user authentication functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,124 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 
+
 function Navbar() {
+    const [showLoginForm, setShowLoginForm] = useState(false);
+    const [showSignUpForm, setShowSignUpForm] = useState(false);
+    const [authenticated, setAuthenticated] = useState(false);
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSignUpClick = () => {
+        setShowLoginForm(false);
+        setShowSignUpForm(!showSignUpForm);
+    };
+
+    const handleLoginClick = () => {
+        setShowSignUpForm(false);
+        setShowLoginForm(!showLoginForm);
+    };
+
+    const handleLoginSubmit = (e) => {
+        e.preventDefault();
+
+        
+        const isValidUser = checkCredentials(username, password);
+
+        if (isValidUser) {
+            console.log(`Logging in with username: ${username}`);
+            setAuthenticated(true);
+            setUsername(username);
+            setPassword('');
+            setShowLoginForm(false);
+            setShowSignUpForm(false);
+        } else {
+            console.error('Invalid username or password. Please try again.');
+        }
+    };
+
+    const handleSignUpSubmit = (e) => {
+        e.preventDefault();
+
+       
+        console.log(`Registering with username: ${username}`);
+       
+        setAuthenticated(true);
+        setUsername(username);
+        setPassword('');
+        setShowSignUpForm(false);
+        setShowLoginForm(true);
+    };
+
+    const handleLogout = () => {
+        setAuthenticated(false);
+        setShowLoginForm(true);
+    };
+
+    const checkCredentials = (enteredUsername, enteredPassword) => {
+        return true;
+    };
+
     return (
         <div className="navbar">
             <div className="logo-container">
                 <h1>CENOMAT</h1>
                 <div className="logo-image"></div>
             </div>
-            <button className="sign-up-button" onClick={handleSignUpClick}>Zaloguj się</button>
+
+            {authenticated ? (
+                <div>
+                    <p>Welcome, {username}!</p>
+                    <button onClick={handleLogout}>Logout</button>
+                </div>
+            ) : (
+                <div>
+                    <button className="sign-up-button" onClick={handleSignUpClick}>
+                        Zarejestruj się
+                    </button>
+                    <button className="sign-up-button" onClick={handleLoginClick}>
+                        Zaloguj się
+                    </button>
+
+                    {(showLoginForm || showSignUpForm) && (
+                        <form
+                            className={showLoginForm ? 'login-form' : 'signup-form'}
+                            onSubmit={showLoginForm ? handleLoginSubmit : handleSignUpSubmit}
+                        >
+                            <label>
+                                Login:
+                                <input
+                                    type="text"
+                                    value={username}
+                                    onChange={(e) => setUsername(e.target.value)}
+                                    required
+                                />
+                            </label>
+                            <label>
+                                Hasło:
+                                <input
+                                    type="password"
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    required
+                                />
+                            </label>
+                            <button type="submit">
+                                {showLoginForm ? 'Zaloguj się' : 'Zarejestruj się'}
+                            </button>
+                        </form>
+                    )}
+                </div>
+            )}
         </div>
     );
 }
 
-function handleSignUpClick() {
-    console.log("Sign Up Clicked!");
-}
-
 export default Navbar;
+
+
+
+
+
+
 
